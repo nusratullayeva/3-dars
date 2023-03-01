7c6700b00efbd2e52845dcd32e9f36e9a8df3923
nusratllayeva <anvarsaidmurotov@gmail.com>
2023-03-01 09:56:38 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let center = 18;
+const user = +prompt('UZINGIZ HAQIDA MALUMOT IRITING')
+
+if (user <= 18) {
+    console.log(`Siz hali uqishingiz kerak`);
+} else {
+    console.log(`siz pensiyaga chiqiwingiz kere`);
+}
+
+let job = 18;
+const acer = +prompt('yowingizni kiriting')
+
+if (acer <= 60) {
+    console.log(`siz mehnat qilishingiz kerak`);
+} else {
+    console.log(`siz dam olishingiz kerak`);
+}
+
+
+let name = 60;
+const notework = +prompt('yowingiz va yilingizni kiriting')
+
+if (notework >= 60) {
+    console.log(`Siz pensiya yoshidasiz pensiyani olib nabiralarizga ham berib turing`);
+} else {
+    console.log(`siz ishlashingiz kerak`);
+}
